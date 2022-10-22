608f1f0060edab6116fe1c53f299cb02e3dc8279
RobertRish <robert.rish1225@gmail.com>
2022-10-22 17:39:53 -0400

    finish dynamic grid made with DOM methods

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const body = document.querySelector("body");
-const grid = document.createElement("div");
-grid.id = "grid"
-body.appendChild(grid);
+const createGrid = document.createElement("div");
+createGrid.id = "grid";
+body.appendChild(createGrid);
+const grid = document.getElementById("grid");
+
 
 
 for (i = 0; i < 4; i++) {
-  const div = document.createElement("div");
-  div.id = `row${i + 1}`;
-  grid.appendChild(div);
+  const createRow = document.createElement("div");
+  createRow.className = "row";
+  createRow.id = `row${i + 1}`;
+  grid.appendChild(createRow);
   const rowDiv = document.getElementById(`row${i + 1}`);
 
   for (j = 0; j < 4; j++) {
     const colDiv = document.createElement("div");
-    colDiv.className = `column${j + 1}`;
+    colDiv.className = 'column';
     rowDiv.appendChild(colDiv);
   };
 };
