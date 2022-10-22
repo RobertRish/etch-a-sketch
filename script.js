38d64866e607db88feaf2d99217d17d9effc7318
RobertRish <robert.rish1225@gmail.com>
2022-10-22 04:24:39 -0400

    create 16 x 16 grid of divs using DOM methods

---
+++ script.js
@@ -0,0 +1,23 @@
+for (i = 0; i < 4; i++) {
+  const div = document.createElement("div");
+  div.id = `row${i + 1}`;
+  const body = document.querySelector("body");
+  body.appendChild(div);
+
+  // write another for loop that, upon each iteration, creates a new div with a
+  // numbered row class and column class
+  // and appendss the new element to the rowDiv of the current
+  // outer for loop iteration
+
+  const rowDiv = document.getElementById(`row${i + 1}`);
+
+  for (j = 0; j < 4; j++) {
+    const colDiv = document.createElement("div");
+    colDiv.className = `column${j + 1}`;
+    rowDiv.appendChild(colDiv);
+  }
+
+  console.log(rowDiv);
+}
+
+// create a 16 x 16 grid of divs using js
