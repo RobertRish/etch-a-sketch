d70c741c16a5f898b2668fcf937a34257fb8bf8d
RobertRish <robert.rish1225@gmail.com>
2022-10-23 05:07:01 -0400

    complete handler function that creates new grid with specified values

--- script.js
+++ script.js
@@ -22,10 +22,39 @@ for (i = 0; i < 4; i++) {
 
 // Makes grid cells change color upon mouse hover
 
-const columnDivs = document.querySelectorAll('.column');
+const columnDivs = document.querySelectorAll(".column");
 
-columnDivs.forEach( col => {
+columnDivs.forEach((col) => {
   col.addEventListener("mouseover", function handleMouseOver(e) {
     col.classList.add("color");
   });
+});
+
+// Adds functionality to button
+
+const button = document.querySelector("button");
+button.addEventListener("click", function handleClick() {
+  const pixels = window.prompt(
+    "Enter the number of pixels per side (it'll be a perfect square so only enter one numerical value) you would like your sketch pad to have.: (Maximum: 100)"
+  );
+  grid.remove();
+  const newGrid = document.createElement("div");
+  newGrid.id = "newGrid";
+  body.appendChild(newGrid);
+  function createNewGrid(pixels) {
+    for (i = 0; i < pixels; i++) {
+      const createRow = document.createElement("div");
+      createRow.className = "row";
+      createRow.id = `row${i + 1}`;
+      newGrid.appendChild(createRow);
+      const rowDiv = document.getElementById(`row${i + 1}`);
+
+      for (j = 0; j < pixels; j++) {
+        const colDiv = document.createElement("div");
+        colDiv.className = "column";
+        rowDiv.appendChild(colDiv);
+      }
+    }
+  }
+  createNewGrid(pixels);
 });
